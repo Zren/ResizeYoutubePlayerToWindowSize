f7e55bcf31e25c30c69de1eb4864c737a5fbb10e
Chris Holland <zrenfire@gmail.com>
2017-03-21 12:23:53 -0400

    Fix top alignment of contents of material page

--- 153699.user.js
+++ 153699.user.js
@@ -632,6 +632,7 @@
             // ytwp.style.appendRule('body > #player:not(.ytd-watch)', 'display', 'none');
             // ytwp.style.appendRule('body.ytwp-viewing-video #content:not(app-header-layout) ytd-page-manager', 'margin-top', '0 !important');
             // ytwp.style.appendRule('.ytd-watch-0 #content-separator.ytd-watch', 'margin-top', '0');
+            ytwp.style.appendRule('ytd-app', 'position', 'static !important');
             ytwp.style.appendRule('ytd-watch #top', 'margin-top', '71px !important'); // 56px (topnav height) + 15px (margin)
             ytwp.style.appendRule('ytd-watch #container', 'margin-top', '0 !important');
             ytwp.style.appendRule('ytd-watch #content-separator', 'margin-top', '0 !important');
